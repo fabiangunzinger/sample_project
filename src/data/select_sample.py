d6c671823417e050f2a94336f2944933620bd765
fabiangunzinger <fa.gunzinger@gmail.com>
2020-10-11 12:48:10 +0100

    Clean up selection.

--- src/data/select_sample.py
+++ src/data/select_sample.py
@@ -52,36 +52,6 @@ def working_age(df):
     return df[age.between(18, 65)]
 
 
-def haciouglu_period(df):
-    """Restrict sample to Jan 2019 - Jun 2020."""
-    return (df.set_index('transaction_date').loc['2019-01':'2020-06']
-            .reset_index())
-
-
-def min_txns_and_spend(df):
-    def helper(g):
-        txns = g.resample('M', on='transaction_date').transaction_id.size()
-        debits = g[g.amount > 0]
-        spend = debits.resample('M', on='transaction_date').amount.sum()
-        return (txns.min() >= 5) & (spend.min() >= 200)
-    return df.groupby('user_id').filter(helper)
-
-
-def income(df):
-    def helper(g):
-        g = g[g.tag.str.match('|'.join(tags))]
-        data2019 = g[g.transaction_date.dt.year.eq(2019)]
-        data2020 = g[g.transaction_date.dt.year.eq(2020)]
-        return (
-            (data2019.transaction_date.dt.month.nunique() >= 8)
-            & (data2020.transaction_date.dt.month.nunique() >= 2)
-            & (data2019[data2019.amount < 0].amount.sum() <= -5000)
-            & (data2019[data2019.amount < 0].amount.sum() > -60000)
-        )
-    tags = ['earnings', 'pensions', 'benefits', 'other income']
-    return df.groupby('user_id').filter(helper)
-
-
 def select_sample(df):
     return (
         df
@@ -92,19 +62,4 @@ def select_sample(df):
         .pipe(diverse_spending)
         .pipe(working_age)
         .pipe(add_count, 'end')
-    )
-
-
-def select_sample_hacioglu(df):
-    return (
-        df
-        .pipe(haciouglu_period)
-        .pipe(current_account)
-        .pipe(min_txns_and_spend)
-        # .pipe(june_refresh)
-        # .pipe(duplicate_accounts)
-        # .pipe(business_accounts)
-        # .pipe(min_expenditure)
-        .pipe(income)
-
     )
